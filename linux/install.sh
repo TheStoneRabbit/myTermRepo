c17bf0fb9ca2473c66346a8eef714be70c93a186
mwl4263 <mwl4263@rit.edu>
2021-12-23 23:10:36 -0500

    started install.sh

---
+++ linux/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# THIS IS A WORK IN PROGRESS
+sudo apt-get install neofetch 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions  
+
